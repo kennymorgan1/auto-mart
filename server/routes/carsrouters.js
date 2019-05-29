78af0c647dbd238a898e3a034947b9ac190c719a
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-05-29 19:59:44 +0100

    update user status [finishes 166337030]

--- server/routes/carsrouters.js
+++ server/routes/carsrouters.js
@@ -6,5 +6,6 @@ import carValidation from '../validators/carsvalidator';
 const router = express.Router();
 
 router.post('/', authenticate, carValidation.postCar, CarsController.postCar);
+router.patch('/:car_id/status', authenticate, carValidation.updateCarStatus, CarsController.updateCarStatus);
 
 export default router;
